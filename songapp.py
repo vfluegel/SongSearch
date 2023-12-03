56a240466315472908fe1935e016423d1ab536f7
Alessandro Colombo <doubleale001@gmail.com>
2023-12-03 19:06:32 +0100

    Ranked retrieval

--- songapp.py
+++ songapp.py
@@ -1,10 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
 import csv
+from whoosh import scoring
 from whoosh.fields import Schema, TEXT
 import os.path
 from whoosh.index import create_in, open_dir
-from whoosh.qparser import QueryParser
+from whoosh.qparser import MultifieldParser
+import shutil
+
+user_feedback = {}
 
 def open_database():
     with open("./song_lyrics.csv", 'r', encoding='utf-8') as file:
@@ -13,38 +17,96 @@ def open_database():
         i = 0
         for row in reader:
             if i <= 100:
-                writer.add_document(title=row['title'], tag=row['tag'], artist=row['artist'], year=row['year'])
+                writer.add_document(title=row['title'], tag=row['tag'], artist=row['artist'], year=row['year'], lyrics=row['lyrics'])
                 i += 1
             else:
                 break
         writer.commit()
-        return
 
 
-def search(query_str, searcher):
-    query = QueryParser("title", ix.schema).parse(query_str)
-    results = searcher.search(query)
+def search(query_str, searcher, search_fields, schema):
+    mparser = MultifieldParser(search_fields, schema)
+    query = mparser.parse(query_str)
+    results = searcher.search(query, limit=100)
     return results
 
+'''
+ Here is the button command, where we could implement a feedback logic ( if possible)
+'''
+def remove_result(result,scores):
+    # Define the logic to remove the result
+    print(f"Remove the result: {result['title']} | Artist: {result['artist']}")
+    user_feedback[result['title']] = result['artist']
+    result_label.config(text="")
+    #display_input()
 
-def display_input():
-    user_input = entry.get()
+'''
+ Second Selection, After the first one selected the songs that belongs to the proper artist/years 
+ We now use the TF-IDF to score the remaining songs only looking at their lyrics, using the temporary schema
+'''
+def second_query(user_input, schema):
+    search_fields = ["lyrics"]
 
-    with ix.searcher() as searcher:
-        results = search(user_input, searcher)
+    with ix.searcher(weighting=scoring.TF_IDF()) as searcher:
+        results = search(user_input, searcher, search_fields, schema)
 
         if results:
-            result_text = "Search Results:\n"
-            for result in results:
-                # Access stored fields directly from the result
-                result_text += f"Title: {result}\n"
+            result_text = ""
+            # save the scores for the documents ( might need for a feedback retrieval??)
+            scores_dict = {(result['title'], result['artist']): result.score for result in results}
+
+            for i, result in enumerate(results):
+                # print
+                result_text += f"{result['title']} | Artist: {result['artist']} | Score: {result.score:.4f}\n"
+
+                # button for the feedback, style sucks I am horrible at design but I didn't want to waste time on moving buttons
+                # plus they don't disappear with sequential queries so they stack. Just don't look to the right while running the app
+                remove_button = ttk.Button(frame,
+                                           text=f"{result['title']} | Artist: {result['artist']}",
+                                           command=lambda r=result: remove_result(r, scores_dict))
+                remove_button.grid(row=i, column=1, sticky=tk.W, padx=(5, 0))
+                if i >= 9:
+                    #Here I retrieved 100 items but only want to print out 10.
+                    #I retrieved 100 to save the scores and use them to fix the list after a feedback, so it's faster
+                    #IMPORTANT!!! feedback is just an idea rn and it's not implemented, so everything related to that is
+                    #just a scratch
+                    break
         else:
-            result_text = "No results found."
+            result_text = "No results found (2)."
 
     result_label.config(text=result_text)
 
+'''
+ First Selection, Must be changed so that it uses a "binary like" logic on dates, artists and tags. 
+ Right now it uses BM25F (default) 
+'''
+def first_query():
+    user_input = entry.get()
+    search_fields = ["title", "tag", "artist", "year", "lyrics"]
 
-schema = Schema(title=TEXT(stored=True), tag=TEXT(stored=True), artist=TEXT(stored=True), year=TEXT(stored=True))
+    with ix.searcher() as searcher:
+        results = search(user_input, searcher, search_fields, ix.schema)
+        if results:
+            # make a new temporary schema for the new query
+            if os.path.exists("tmp_index"):
+                shutil.rmtree("tmp_index")
+            os.mkdir("tmp_index")
+            create_in("tmp_index", tmp_schema)
+            tmp_ix = open_dir("tmp_index")
+            tmp_writer = tmp_ix.writer()
+            for result in results:
+                tmp_writer.add_document(title=result['title'], tag=result['tag'], artist=result['artist'], year=result['year'], lyrics=result['lyrics'])
+            # run the second query
+            second_query(user_input, tmp_ix.schema)
+        else:
+            result_text = "No results found (1)."
+            result_label.config(text=result_text)
+    return
+
+'''
+First schema, with all the songs
+'''
+schema = Schema(title=TEXT(stored=True), tag=TEXT(stored=True), artist=TEXT(stored=True), year=TEXT(stored=True), lyrics=TEXT(stored=True))
 if not os.path.exists("index"):
     os.mkdir("index")
     create_in("index", schema)
@@ -53,7 +115,16 @@ if not os.path.exists("index"):
 else:
     ix = open_dir("index")
 
+'''
+Second schema, that is emptied each time. 
+Here we save only the songs that survive to the first scan ( aka binary search ) 
+and so we scan this schema for the TF-IDF algo
+'''
+tmp_schema = Schema(title=TEXT(stored=True), tag=TEXT(stored=True), artist=TEXT(stored=True), year=TEXT(stored=True), lyrics=TEXT(stored=True))
 
+'''
+PANEL and STYLE
+'''
 app = tk.Tk()
 app.title("Song App")
 
@@ -69,7 +140,7 @@ prompt_label.grid(column=0, row=0, pady=(0, 5), sticky=tk.W)
 entry = ttk.Entry(frame, width=60)
 entry.grid(column=0, row=1, pady=(0, 10), sticky=tk.W)
 
-submit_button = ttk.Button(frame, text="Submit", command=display_input)
+submit_button = ttk.Button(frame, text="Submit", command=first_query)
 submit_button.grid(column=0, row=2, pady=(0, 10), sticky=tk.W)
 
 result_label = ttk.Label(frame, text="")
